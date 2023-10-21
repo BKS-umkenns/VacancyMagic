0fc2e4553f7d539db9dc02307cc68921a5a0b00c
temerk1n <nikita.verigin23@gmail.com>
2023-10-22 01:01:34 +0300

    feat: landing ServiceCard.vue

--- BKS.VacancyMagic.Frontend/vacancy/src/Pages/Landing.vue
+++ BKS.VacancyMagic.Frontend/vacancy/src/Pages/Landing.vue
@@ -1,19 +1,107 @@
 <template>
-  <NavBar/>
-  <div>
-    Landing Page - there will be some advertisement
+  <NavBar />
+  <div class="page">
+    <div class="content">
+      <div class="serviceCardList">
+        <div v-for="service in services">
+          <ServiceCard :service="service" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import NavBar from "../components/NavBar.vue";
+import ServiceCard from "../components/ServiceCard.vue";
 
 export default {
   name: "Home",
-  components: {NavBar}
+  components: {ServiceCard, NavBar},
+  data() {
+    return {
+      services: [
+        {
+          id: "hh",
+          name: "HH",
+          logoUrl: '/hhLogo.png',
+          color: "red",
+          functions: [
+            {
+              name: "Поиск работы",
+              implemented: true,
+            },
+            {
+              name: "Отслеживание откликов",
+              implemented: true,
+            }
+          ],
+        },
+        {
+          id: "hh",
+          name: "HH",
+          logoUrl: '/hhLogo.png',
+          color: "red",
+          functions: [
+            {
+              name: "Поиск работы",
+              implemented: true,
+            },
+            {
+              name: "Отслеживание откликов",
+              implemented: false,
+            }
+          ]
+        },
+        {
+          id: "hh",
+          name: "HH",
+          logoUrl: '/hhLogo.png',
+          color: "red",
+          functions: [
+            {
+              name: "Поиск работы",
+              implemented: true,
+            },
+            {
+              name: "Отслеживание откликов",
+              implemented: false,
+            }
+          ]
+        },
+      ],
+    }
+  },
 }
 </script>
 
 <style scoped>
+.page {
+  background-color: #f8f8f8;
 
+  padding-top: 2rem;
+
+  height: 100vh;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+}
+.content {
+  width: 100%;
+}
+.serviceCardList {
+  width: 70%;
+
+  margin: auto;
+
+  display: grid;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+  gap: 2rem;
+}
+@media screen and (max-width: 65rem){
+  .serviceCardList {
+    grid-template-columns: repeat(1, minmax(0, 1fr));
+  }
+}
 </style>
---
+++ BKS.VacancyMagic.Frontend/vacancy/src/components/ServiceCard.vue
@@ -0,0 +1,78 @@
+<template>
+  <va-card
+    :id="service.id"
+    :style="{ boxShadow: `-10px -10px 2px ${service.color}` }"
+  >
+    <va-card-content class="content">
+      <div class="title" role="heading">
+        {{service.name}}
+        <va-image
+          class="service-logo"
+          fit="contain"
+          :src="service.logoUrl"
+        />
+      </div>
+      <div class="function" v-for="func in service.functions">
+        <p :class="func.implemented ? '' : 'soon'">{{ func.name }}</p>
+        <va-image
+          v-if="func.implemented"
+          class="checkedIcon"
+          fit="contain"
+          src="/checked.png"
+        />
+        <span v-else>Скоро</span>
+      </div>
+    </va-card-content>
+  </va-card>
+</template>
+
+<script>
+import TagsContainer from "./TagsContainer.vue";
+
+export default {
+  name: "ServiceCard",
+  components: {TagsContainer},
+  props: {
+    service: {},
+  },
+}
+</script>
+
+<style scoped>
+.content {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+}
+.title {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  gap: 1rem;
+  margin-bottom: 1rem;
+
+  align-items: center;
+
+  font-size: 1.25rem;
+}
+.service-logo {
+  width: 2.5rem;
+  height: 2.5rem;
+}
+.checkedIcon {
+  width: 1.4rem;
+  height: 1.4rem;
+}
+.function {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  gap: 1rem;
+
+  line-height: 1rem;
+  font-size: 1rem;
+}
+.soon {
+  color: #adb3b9;
+}
+</style>
