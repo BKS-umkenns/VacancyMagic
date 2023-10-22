9df845e78ecf1df19478faa4bec3c2a68375ee3e
Vlad Verigin <vladver233@gmail.com>
2023-10-22 03:47:58 +0300

    dev mode update

--- BKS.VacancyMagic.Frontend/vacancy/vite.config.js
+++ BKS.VacancyMagic.Frontend/vacancy/vite.config.js
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    base: command === 'serve' ? "" : "/test-cloud-frontend/",
+    server: {
+      host: true,
+      port: 4173, // This is the port which we will use in docker
+      watch: {
+        usePolling: true
+      },
+      proxy: {
+        '/api': {
+          target: 'http://localhost:5050/',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        }
+      }
+    }
+  }
+});
